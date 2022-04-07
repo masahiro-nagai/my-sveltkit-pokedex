2665cd6728bedc9a8449ed06eee23236ced0d3f3
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-04-08 00:05:13 +0900

    pokemanCard作成

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
 	import {pokemon} from "../stores/pokestore";
+	import PokemanCard from "../components/pokemanCard.svelte"
 	console.log($pokemon)
 </script>
 <svelte:head>
@@ -7,7 +8,8 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
-
-{#each $pokemon as pokeman}
-<p>{pokeman.name}</p>
-{/each}
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each $pokemon as pokeman}
+	<PokemanCard {pokeman}/>
+	{/each}
+</div>
