90f635327d273bbfd7c5d97db820b4e38e24706c
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-04-09 01:23:45 +0900

    検索機能実装

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,28 @@
 <script>
 	import {pokemon} from "../stores/pokestore";
 	import PokemanCard from "../components/pokemanCard.svelte"
-	console.log($pokemon)
+	
+	let searchTerm ="";
+	let filteredPokemon = [];
+
+	$:{
+		console.log(searchTerm);
+		if(searchTerm){
+			//serch pokemon
+			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		}else {
+			filteredPokemon = [...$pokemon]
+		}
+	}
 </script>
 <svelte:head>
 	<title>Svelte Kit Pokedex</title>
 </svelte:head>
 
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each $pokemon as pokeman}
+	{#each filteredPokemon as pokeman}
 	<PokemanCard {pokeman}/>
 	{/each}
 </div>
