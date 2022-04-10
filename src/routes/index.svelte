b486404035c7bbd7c6b1f9bd21401c461dd76543
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-04-11 00:32:27 +0900

    ページ生成エラー修正できず

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,27 @@
 <script>
-	import {pokemon} from "../stores/pokestore";
-	import PokemanCard from "../components/pokemanCard.svelte"
-	
-	let searchTerm ="";
+	import PokemanCard from "../components/pokemanCard.svelte";
+	import {pokemon, fetchPokemon} from "../stores/pokestore";
+	let searchTerm = "";
 	let filteredPokemon = [];
-
-	$:{
-		console.log(searchTerm);
+	$: {
 		if(searchTerm){
-			//serch pokemon
-			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
-		}else {
-			filteredPokemon = [...$pokemon]
+			filteredPokemon = $pokemon.filter( pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		}
+		else {
+			filteredPokemon = [...$pokemon];
 		}
 	}
-</script>
-<svelte:head>
-	<title>Svelte Kit Pokedex</title>
-</svelte:head>
-
-<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
-<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each filteredPokemon as pokeman}
-	<PokemanCard {pokeman}/>
-	{/each}
-</div>
+	fetchPokemon();
+	</script>
+	<svelte:head>
+		<title>Pokedex</title>
+	</svelte:head>
+	
+	<h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+	<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Pokemon">
+	<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+		
+		{#each filteredPokemon as pokeman}
+				<PokemanCard pokeman={pokeman}/>
+		{/each}
+	</div>
